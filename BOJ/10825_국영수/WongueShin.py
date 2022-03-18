3b3134a8015edbf219dd95afc0dbbf4802253eb0
WongueShin <won7963kr@gamil.com>
2022-03-18 16:36:11 +0900

    Solved: 10825_국영수 tag: sort

---
+++ BOJ/10825_국영수/WongueShin.py
@@ -0,0 +1,14 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def solution():
+    N = int(input())
+    studentList = [(lambda x:(x[0], int(x[1]), int(x[2]), int(x[3])))(input().split(' '))  for _ in range(N)]
+    studentList.sort(key= lambda x: (-x[1], x[2], -x[3], x[0]))
+    for i in range(N):
+        print(studentList[i][0])
+    
+
+solution()
